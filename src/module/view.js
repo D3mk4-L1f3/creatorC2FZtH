a85c59edc6b012760b80a0c89302109f4f46f513
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-09-01 12:40:25 +0300

    bugfixed svg changed js render

--- src/module/view.js
+++ src/module/view.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
+import sprite from '../public/sprite.svg';
 
 const BASE_URL = 'https://drinkify-backend.p.goit.global/API/V1/cocktails';
 const refs = {
   cocktailList: document.querySelector('.cocktail-list'),
 };
 
-async function getCocktails() {
-  const res = await axios.get(`${BASE_URL}?r=8`);
+const logo = new URL('../public/sprite.svg#heart', import.meta.url);
+// const logo = new URL(`${heart}`, import.meta.url);
+
+function isScreenMobile() {
+  return window.matchMedia('(max-width: 1279px)').matches;
+}
+
+export default async function getCocktails() {
+  let totalCocktails = 8;
+  if (!isScreenMobile()) {
+    totalCocktails = 9;
+  }
+
+  const res = await axios.get(`${BASE_URL}?r=${totalCocktails}`);
 
   const arrCocktails = await Promise.all(
     res.data.map(async cocktail => {
@@ -16,7 +29,12 @@ async function getCocktails() {
       return response.data;
     })
   );
-  const markup = arrCocktails.flat().map(createMarkup).join('');
+
+  renderMarkUp(arrCocktails);
+}
+
+function renderMarkUp(arr) {
+  const markup = arr.flat().map(createMarkup).join('');
   refs.cocktailList.insertAdjacentHTML('beforeend', markup);
 }
 
@@ -28,6 +46,7 @@ function createMarkup({ drinkThumb, description, drink, _id }) {
             class="card-image img"
             src="${drinkThumb}"
             alt="${drink}"
+            loading="lazy"
           />
         <div class="text-box">
           <h2 class="cocktail-name">${drink}</h2>
@@ -38,7 +57,7 @@ function createMarkup({ drinkThumb, description, drink, _id }) {
             </button>
             <button type="button" class="btn-favorite btn js-btn-favorite" data-id="${_id}">
               <svg class="card-icon">
-                <use href="./public/sprite.svg#heart"></use>
+                <use href="${sprite}#heart"></use>;
               </svg>
             </button>`;
 }
