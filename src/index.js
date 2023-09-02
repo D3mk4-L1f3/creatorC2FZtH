98cce2a139169afdd12393d2f9a649b072685845
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-09-02 12:46:10 +0200

    add modal auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './module/header.js';
 import './module/localStorage.js';
+<<<<<<< Updated upstream
 import './module/view.js';
 import './module/hero.js';
 import './module/upperSearchNav.js';
@@ -8,3 +9,14 @@ import './module/favorite.js';
 import './module/pagination.js';
 import './module/modal.js';
 import './module/dark.js';
+=======
+import './module/hero.js';
+import './module/modal.js';
+import './module/upperSearchNav.js';
+import './module/select.js';
+import './module/favorite.js';
+import './module/view.js';
+import './module/underSearchNavigation.js';
+import './module/dark.js';
+import './module/auth.js';
+>>>>>>> Stashed changes
