34c3b9510fe7291f72088ca561285ced7aa03b01
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-09-02 16:38:16 +0300

    update
    -add var to base.css for input outline color;
    -add logic for select js like in upperSearch js;
    -refactored upperSearch.js;
    -added anchor-link for footer logo to header, for better UX;
    -

--- src/module/upperSearchNav.js
+++ src/module/upperSearchNav.js
@@ -2,45 +2,12 @@ import axios from 'axios';
 import { renderMarkUp } from './view';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// ===========================================================================
 
 const BASE_URL = 'https://drinkify-backend.p.goit.global/api/v1';
 const END_POINT = '/cocktails/search/';
 
-let refs = {
-  searchForm: document.querySelector('.js-search'),
-  cockList: document.querySelector('.cocktail-list'),
-  burgerMenu: document.querySelector('.js-open-menu'),
-  burgerCloseBtn: document.querySelector('.js-close-menu'),
-  mobileMenu: document.querySelector('.js-menu-container'),
-  burgerSearchForm: document.querySelector('#burger-search-form'),
-};
-// ===========================================================================
-refs.burgerMenu.addEventListener('click', onBurgerMenuSearch);
-refs.burgerCloseBtn.addEventListener('click', onBurgerMenuClose);
-// ===========================================================================
-
-function onBurgerMenuSearch(e) {
-  refs.burgerSearchForm.addEventListener(
-    'input',
-    debounce(onSearchFormInput, 1000)
-  );
-}
-
-function onBurgerMenuClose() {
-  refs.burgerMenu.removeEventListener('click', onBurgerMenuSearch);
-  refs.burgerCloseBtn.removeEventListener('click', onBurgerMenuClose);
-}
-
-function isScreenMobile() {
-  return window.matchMedia('(max-width: 1279px)').matches;
-}
-
-let totalCocktails = 8;
-if (!isScreenMobile()) {
-  totalCocktails = 9;
-}
-
-async function fetchCocktailsName(name) {
+async function fetchCocktailsbyName(name) {
   const PARAMS = new URLSearchParams({
     s: name,
   });
@@ -57,6 +24,7 @@ async function fetchCocktailsName(name) {
     const section = document.getElementById('cocktail-section');
     const distance = section.getBoundingClientRect().top;
     window.scrollBy(0, distance);
+
     Notify.success(`We found ${cocktailDetail.length} cocktails!`, {
       position: 'center-top',
       clickToClose: true,
@@ -64,7 +32,6 @@ async function fetchCocktailsName(name) {
       width: 'fit-content',
     });
   } catch (err) {
-    console.log(err);
     Notify.failure(`Error: Unable to find cocktail ${name}`, {
       clickToClose: true,
       fontSize: '22px',
@@ -73,13 +40,46 @@ async function fetchCocktailsName(name) {
   }
 }
 
+const refs = {
+  searchForm: document.querySelector('.js-search'),
+  cockList: document.querySelector('.cocktail-list'),
+  burgerMenu: document.querySelector('.js-open-menu'),
+  burgerCloseBtn: document.querySelector('.js-close-menu'),
+  mobileMenu: document.querySelector('.js-menu-container'),
+  burgerSearchForm: document.querySelector('#burger-search-form'),
+};
+// ===========================================================================
+refs.burgerMenu.addEventListener('click', onBurgerMenuSearch);
+refs.burgerCloseBtn.addEventListener('click', onBurgerMenuClose);
 refs.searchForm.addEventListener('input', debounce(onSearchFormInput, 1000));
+// ===========================================================================
+
+function onBurgerMenuSearch() {
+  refs.burgerSearchForm.addEventListener(
+    'input',
+    debounce(onSearchFormInput, 1000)
+  );
+}
+
+function onBurgerMenuClose() {
+  refs.burgerMenu.removeEventListener('click', onBurgerMenuSearch);
+  refs.burgerCloseBtn.removeEventListener('click', onBurgerMenuClose);
+}
 
 function onSearchFormInput(e) {
   e.preventDefault();
   const name = e.target.value;
-  fetchCocktailsName(name);
+  fetchCocktailsbyName(name);
   refs.searchForm.reset();
   onBurgerMenuClose();
   refs.burgerSearchForm.reset();
+}
+
+let totalCocktails = 8;
+if (!isScreenMobile()) {
+  totalCocktails = 9;
+}
+
+function isScreenMobile() {
+  return window.matchMedia('(max-width: 1279px)').matches;
 }
